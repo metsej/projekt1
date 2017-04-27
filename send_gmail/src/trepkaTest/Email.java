18a26e64d247e1a95ead1dc388a8653a6add2c04
Grzegorz Klimek <grzegorzklimek77@gmail.com>
2017-04-27 19:44:41 +0200

    *

--- send_gmail/src/trepkaTest/Email.java
+++ send_gmail/src/trepkaTest/Email.java
@@ -1,14 +1,8 @@
 package trepkaTest;
 
-import java.util.ArrayList;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Email {
 
