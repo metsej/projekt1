9a738aade8c9ede081621926cc961a5d3eb864e6
Grzegorz Klimek <grzegorzklimek77@gmail.com>
2017-05-11 15:52:11 +0200

    Dodano zapisywanie początku i konca czasu pracy i parę innych

--- applikacja_dla_pracownikow/src/applikacja_dla_pracownikow/Administrator.java
+++ applikacja_dla_pracownikow/src/applikacja_dla_pracownikow/Administrator.java
@@ -35,7 +35,6 @@ public class Administrator {
 			
 		}
 	
-	
 	public static String generujHaslo () {
 		int randomNum = ThreadLocalRandom.current().nextInt(0, 15 );
 		String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
