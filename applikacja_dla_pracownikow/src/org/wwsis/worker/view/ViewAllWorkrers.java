ae902c9bf5f28204a2cbd3746714bfd1c6d11068
Grzegorz Klimek <grzegorzklimek77@gmail.com>
2017-05-16 16:59:05 +0200

    *

--- applikacja_dla_pracownikow/src/org/wwsis/worker/view/ViewAllWorkrers.java
+++ applikacja_dla_pracownikow/src/org/wwsis/worker/view/ViewAllWorkrers.java
@@ -55,7 +55,7 @@ public class ViewAllWorkrers extends JFrame {
 	public ViewAllWorkrers( AppController contr) {
 		
 		this.controller = contr;
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 950, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
