5260b1db4f5f66c8fea2a55332cfc1f33b94e4d7
Grzegorz Klimek <grzegorzklimek77@gmail.com>
2017-05-20 02:26:20 +0200

    *

--- applikacja_dla_pracownikow/src/org/wwsis/worker/view/MainMenu.java
+++ applikacja_dla_pracownikow/src/org/wwsis/worker/view/MainMenu.java
@@ -34,21 +34,20 @@ import java.awt.Rectangle;
 
 public class MainMenu {
 
-	public  JFrame frame;
+	public JFrame frame;
 	private JTextField loginTextField;
 	private AppController controller;
 	private JPasswordField passwordField;
 	private static Rectangle bounds;
 	private static boolean wasSomeWindowDisplayed = false;
 	private JButton logInButton;
-	/**
-	 * Launch the application.
-	 */
+
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainMenu window = new MainMenu();
+					MainMenu window = new MainMenu(null);
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -56,66 +55,63 @@ public class MainMenu {
 			}
 		});
 	}
-	/**
-	 * Create the application.
-	 */
+
 	public MainMenu(AppController contr) {
 		this.controller = contr;
 		initialize();
 	}
-	
-	public MainMenu() {
-		initialize();
-	}
-	/**
-	 * Initialize the contents of the frame.
-	 */
+
 	private void initialize() {
+
+		setFrame();
+
+		loginTextField = new JTextField();
+		loginTextField.setBounds(149, 127, 261, 46);
+		frame.getContentPane().add(loginTextField);
+		loginTextField.setColumns(10);
+
+		setLoginActionListener();
+
+		JLabel lblLogin = new JLabel("           Login");
+		lblLogin.setBounds(52, 127, 99, 45);
+		frame.getContentPane().add(lblLogin);
+
+		JLabel lblPassword = new JLabel("  Password");
+		lblPassword.setBounds(62, 184, 89, 37);
+		frame.getContentPane().add(lblPassword);
+
+		passwordField = new JPasswordField();
+		passwordField.setBounds(149, 185, 261, 46);
+		frame.getContentPane().add(passwordField);
+
+	}
+
+	private void setFrame() {
 		frame = new JFrame("Main menu");
-		if (!wasSomeWindowDisplayed){
+		if (!wasSomeWindowDisplayed) {
 			frame.setBounds(100, 100, 564, 415);
 		} else {
 			frame.setBounds(bounds);
 			frame.setSize(564, 415);
 		}
-		
+
 		frame.getBounds();
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setResizable(false);
 		wasSomeWindowDisplayed = true;
-		
+
 		frame.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosed(WindowEvent e) {
-					
+
 				bounds = frame.getBounds();
 			}
 		});
-		
-		loginTextField = new JTextField();
-		loginTextField.setBounds(149, 127, 261, 46);
-		frame.getContentPane().add(loginTextField);
-		loginTextField.setColumns(10);
-		
-		setLoginActionListener();
-		
-		
-		JLabel lblLogin = new JLabel("           Login");
-		lblLogin.setBounds(52, 127, 99, 45);
-		frame.getContentPane().add(lblLogin);
-		
-		JLabel lblPassword = new JLabel("  Password");
-		lblPassword.setBounds(62, 184, 89, 37);
-		frame.getContentPane().add(lblPassword);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(149, 185, 261, 46);
-		frame.getContentPane().add(passwordField);
-		
+
 	}
-	
-	public void setLoginActionListener() {
+
+	private void setLoginActionListener() {
 		logInButton = new JButton("Log in");
 		logInButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -123,7 +119,7 @@ public class MainMenu {
 					String login = loginTextField.getText();
 					@SuppressWarnings("deprecation")
 					String pass = passwordField.getText();
-					
+
 					if (controller.isAdmin(login, pass)) {
 
 						MainMenu.setBounds(frame.getBounds());
@@ -131,7 +127,7 @@ public class MainMenu {
 						frame.dispose();
 						AdministratorPanel adminPanel = new AdministratorPanel(controller);
 						adminPanel.setVisible(true);
-						
+
 					} else if (controller.isValidLogNPass(pass, login)) {
 						MainMenu.setBounds(frame.getBounds());
 						frame.dispose();
@@ -139,27 +135,27 @@ public class MainMenu {
 						controller.saveStartTime(loggedWorker);
 						WorkerPanel workerPanel = new WorkerPanel(loggedWorker, controller);
 						workerPanel.setVisible(true);
-					
+
 					} else {
-						JOptionPane.showMessageDialog(null, "wrong password or login", "Alert", JOptionPane.WARNING_MESSAGE);
+						JOptionPane.showMessageDialog(null, "wrong password or login", "Alert",
+								JOptionPane.WARNING_MESSAGE);
 					}
-					
-					} catch (Exception exc) {
-					
+
+				} catch (Exception exc) {
+
 				}
 			}
 		});
 		logInButton.setBounds(221, 273, 117, 25);
 		frame.getContentPane().add(logInButton);
 	}
-	
-	public static Rectangle getBounds( ) {
+
+	public static Rectangle getBounds() {
 		return bounds;
 	}
-	
-	public static void setBounds (Rectangle b) {
+
+	public static void setBounds(Rectangle b) {
 		bounds = b;
 	}
-	
 
 }
--- applikacja_dla_pracownikow/src/org/wwsis/worker/view/WorkerPanel.java
+++ applikacja_dla_pracownikow/src/org/wwsis/worker/view/WorkerPanel.java
@@ -23,6 +23,12 @@ public class WorkerPanel extends JFrame {
 	private JPanel contentPane;
 	private AppController controller;
 	private Worker loggedWorker;
+	private JButton btnLogOut;
+	private JLabel lblLoggedAs;
+	private JLabel loginDisplay;
+	private JLabel lblLastTimeStart;
+	private JLabel sWorkTimeLabbelDisplay;
+	private JButton btnNewButton;
 
 	/**
 	 * Launch the application.
@@ -40,9 +46,7 @@ public class WorkerPanel extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	
 	public WorkerPanel(Worker logWor, AppController contr) {
 	
 		this.controller = contr;
@@ -54,7 +58,6 @@ public class WorkerPanel extends JFrame {
 
 					controller.logOut(loggedWorker);
 					MainMenu.setBounds(getBounds());
-				
 			}
 		});
 		
@@ -68,35 +71,13 @@ public class WorkerPanel extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblLoggedAs = new JLabel("Logged as: ");
-		lblLoggedAs.setBounds(12, 35, 89, 33);
-		contentPane.add(lblLoggedAs);
+		setLabelsAndButtons ();
+		setLogOutActionListener();
 		
-		JLabel loginDisplay = new JLabel(loggedWorker.getLogin());
-		loginDisplay.setBounds(100, 35, 269, 33);
-		contentPane.add(loginDisplay);
 		
-	
-		JLabel lblLastTimeStart = new JLabel("Last time start work at: ");
-		lblLastTimeStart.setBounds(12, 80, 177, 33);
-		contentPane.add(lblLastTimeStart);
-		
-		String timeStart;
-		if (loggedWorker.getStartTime() == null) {
-			timeStart = " Haven't worked yet";
-		} else {
-			timeStart = loggedWorker.getStartTime();
-		}
-		
-		JLabel sWorkTimeLabbelDisplay = new JLabel(timeStart);
-		sWorkTimeLabbelDisplay.setBounds(204, 80, 231, 33);
-		contentPane.add(sWorkTimeLabbelDisplay);
-		
-		JButton btnNewButton = new JButton("Start Work");
-		btnNewButton.setBounds(48, 306, 117, 25);
-		contentPane.add(btnNewButton);
-		
-		JButton btnLogOut = new JButton("Log out");
+	}
+	private void setLogOutActionListener() {
+		btnLogOut = new JButton("Log out");
 		btnLogOut.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				controller.logOut(loggedWorker);
@@ -109,6 +90,38 @@ public class WorkerPanel extends JFrame {
 		});
 		btnLogOut.setBounds(379, 306, 117, 25);
 		contentPane.add(btnLogOut);
+		
+	}
+	
+	private void setLabelsAndButtons () {
+		lblLoggedAs = new JLabel("Logged as: ");
+		lblLoggedAs.setBounds(12, 35, 89, 33);
+		contentPane.add(lblLoggedAs);
+		
+		 loginDisplay = new JLabel(loggedWorker.getLogin());
+		loginDisplay.setBounds(100, 35, 269, 33);
+		contentPane.add(loginDisplay);
+		
+	
+		 lblLastTimeStart = new JLabel("Last time start work at: ");
+		lblLastTimeStart.setBounds(12, 80, 177, 33);
+		contentPane.add(lblLastTimeStart);
+		
+		String timeStart;
+		if (loggedWorker.getStartTime() == null) {
+			timeStart = " Haven't worked yet";
+		} else {
+			timeStart = loggedWorker.getStartTime();
+		}
+		
+		sWorkTimeLabbelDisplay = new JLabel(timeStart);
+		sWorkTimeLabbelDisplay.setBounds(204, 80, 231, 33);
+		contentPane.add(sWorkTimeLabbelDisplay);
+		
+		JButton btnNewButton = new JButton("Start Work");
+		btnNewButton.setBounds(48, 306, 117, 25);
+		contentPane.add(btnNewButton);
+		
 	}
 
 }
