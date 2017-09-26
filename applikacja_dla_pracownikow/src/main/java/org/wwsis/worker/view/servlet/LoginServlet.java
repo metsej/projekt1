cfb2db9df31f98f9a62f710dfa8f9a01e990d4c1
Grzegorz Klimek <grzegorzklimek77@gmail.com>
2017-09-26 20:40:55 +0200

    *

--- applikacja_dla_pracownikow/src/main/java/org/wwsis/worker/view/servlet/LoginServlet.java
+++ applikacja_dla_pracownikow/src/main/java/org/wwsis/worker/view/servlet/LoginServlet.java
@@ -2,8 +2,10 @@ package org.wwsis.worker.view.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,4 +23,12 @@ public class LoginServlet extends HttpServlet {
 		out.print("Hello World");
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doPost(req, resp);
+		Map<String, String[]> parametry = req.getParameterMap();
+		resp.addCookie(new Cookie("SID","asdfadfwefwef"));
+		System.out.println("Próba loginu :"+parametry.get("login")[0]+" "+parametry.get("password")[0]);
+	}
 }
