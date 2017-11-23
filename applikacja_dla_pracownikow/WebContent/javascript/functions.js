bc8fd124479940d892baf0f71849445c900375c4
Grzegorz Klimek <grzegorzklimek77@gmail.com>
2017-11-23 21:12:38 +0100

    *

--- applikacja_dla_pracownikow/WebContent/javascript/functions.js
+++ applikacja_dla_pracownikow/WebContent/javascript/functions.js
@@ -1,3 +1,28 @@
+function submitSignInData(){
+    var userValue = $("#userInput").val();
+    var passwordValue = $("#passInput").val();
+    var userFieldName = "user";
+    var passwordFieldName = "password";
+
+    var successResp = "success";
+    var userIsBlockedResp = "user is blocked";
+
+    var emitJSON = {};
+    emitJSON[userFieldName] = userValue;
+    emitJSON[passwordFieldName] = passwordValue ;
+
+    $.post('/login', emitJSON,
+        function(returnedData){
+            if (returnedData === successResp) {
+                $(location).attr("href", "/login");
+            } else if (returnedData === userIsBlockedResp ) {
+                alert("Trzykrotnie podales zle haslo. Twoje konto jest zablokowane. W celu odblokowania skontaktuj sie z administratorem");
+            } else {
+                alert ("Zle haslo lub login");
+            }
+        });
+}
+
 
 function submitSignInData(){
 
